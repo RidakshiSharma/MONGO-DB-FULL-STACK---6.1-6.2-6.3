37e65f589d7d9a3bfc8ef2d016aa3e3097902521
RidakshiSharma <sonikasharma8141@gmail.com>
2025-10-29 22:52:58 +0530

    Initialize App component with ProductList

---
+++ backend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductList from './components/ProductList'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial', margin: '40px' }}>
+      <h1>Product Store</h1>
+      <ProductList />
+    </div>
+  )
+}
+
+export default App
